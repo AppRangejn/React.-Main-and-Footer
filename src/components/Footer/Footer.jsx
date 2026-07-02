08efd0ddad34526755df69f1f84a3099fe7d4418
Kiril <kiril0302ua@gmail.com>
2026-07-02 17:23:30 +0300

    add header, main, footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className="footer-info">
+      <h4>АДРЕСА КІНОКОМПАНІЇ:</h4>
+      <p>Netflix Studios, Sunset Blvd 5800, Los Angeles, USA</p>
+      <h4>EMAIL РЕЖИСЕРА:</h4>
+      <p><a href="mailto:onePieceDirector@netflix.com">director@netflix.com</a></p>
+      <h4>ТЕЛЕФОН АВТОРА САЙТУ</h4>
+      <p><a href="tel:+380969851708">+380 (96) 985-17-08</a></p>
+    </div>  
+
+  )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="title-header">
+      <h1>NETFLIX</h1>
+    </div>
+  )
+}
+
+export default Header
